80b34bff94b066709c3181948aea2ad2ffdbf7d3
Malindu4 <malindumahakumara@gmail.com>
2025-06-11 11:55:07 +0530

    init❤️

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import ProductCard from './components/ProductCard'
+
+function App() {
+ 
+
+  return (
+    <>
+    <ProductCard name ="audio setup "price="3250/"description="Audio setup" 
+    img="https://www.pngall.com/wp-content/uploads/2016/05/Headphones-PNG-Image.png"/>
+     <ProductCard name ="audio setup "prosp="3250/"description="Audio setup"> </ProductCard>
+    </>
+  )
+}
+
+export default App
